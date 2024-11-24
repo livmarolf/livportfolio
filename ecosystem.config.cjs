1376ec1cb5b82119a84c5aa1a144a510d34efd72
Justin Taddei <justin@Mac.utahbroadband.com>
2024-11-24 13:21:09 -0700

    fix pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: 'NuxtAppName',
-      port: '3005',
+      name: 'livmarolf',
+      port: '3003',
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs'
